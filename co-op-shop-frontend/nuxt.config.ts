d66e3e5e384d48266e0fcc307ad591f429b1922f
Jordan Blackbourn <blackbournj@gmail.com>
2022-08-16 22:41:44 +1200

    create backend pachage, install vuetify, trpc-nuxt

--- co-op-shop-frontend/nuxt.config.ts
+++ co-op-shop-frontend/nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: ['trpc-nuxt'],
+  trpc: {
+    baseURL: 'http://localhost:3000', // defaults to http://localhost:3000
+    endpoint: '/trpc', // defaults to /trpc
+  },
+  typescript: {
+    strict: true // required to make input/output types work
+  }
 })
