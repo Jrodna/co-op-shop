e0ef8a7c32c37cbd821065734af273c3957ce324
Jordan Blackbourn <blackbournj@gmail.com>
2022-08-17 22:47:02 +1200

    setup prettier and eslint

--- co-op-shop-frontend/nuxt.config.ts
+++ co-op-shop-frontend/nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
+import {defineNuxtConfig} from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -8,6 +8,6 @@ export default defineNuxtConfig({
     endpoint: '/trpc', // defaults to /trpc
   },
   typescript: {
-    strict: true // required to make input/output types work
-  }
-})
+    strict: true, // required to make input/output types work
+  },
+});
